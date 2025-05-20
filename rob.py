35d0f88f40333d0ca99621a454400bebf382c8dd
Robinson lopez <robinsonlopez1199@gmail.com>
2025-05-20 05:35:58 -0500

    Update rob.py

--- rob.py
+++ rob.py
@@ -10,7 +10,7 @@ game_state = {
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("rob.html")
 
 @app.route("/choose_color", methods=["POST"])
 def choose_color():
