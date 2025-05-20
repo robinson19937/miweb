fc1d4bfe80f5081b39882a7c3e5159c683e239c6
Robinson lopez <robinsonlopez1199@gmail.com>
2025-05-20 05:44:33 -0500

    Update rob.py

--- rob.py
+++ rob.py
@@ -1,3 +1,6 @@
+
+import os
+
 from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
@@ -23,5 +26,7 @@ def restart():
     game_state["fen"] = "startpos"
     return jsonify(success=True)
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
