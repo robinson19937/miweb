998e7ff77a9b50a0224b42afafcb1a58161da9c7
Robinson lopez <robinsonlopez1199@gmail.com>
2025-05-20 05:33:01 -0500

    Create rob.js

---
+++ rob.js
@@ -0,0 +1,40 @@
+let boardSize = 480;
+let tileSize = boardSize / 8;
+
+function setup() {
+  let canvas = createCanvas(boardSize, boardSize);
+  canvas.parent("board-canvas");
+  noLoop();
+  drawBoard();
+}
+
+function drawBoard() {
+  for (let y = 0; y < 8; y++) {
+    for (let x = 0; x < 8; x++) {
+      if ((x + y) % 2 === 0) {
+        fill(240);
+      } else {
+        fill(100);
+      }
+      rect(x * tileSize, y * tileSize, tileSize, tileSize);
+    }
+  }
+}
+
+function chooseColor(color) {
+  fetch('/choose_color', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ color: color })
+  }).then(() => {
+    console.log("Color elegido:", color);
+  });
+}
+
+function restartGame() {
+  fetch('/restart', { method: 'POST' })
+    .then(() => {
+      console.log("Juego reiniciado");
+      drawBoard();
+    });
+}
