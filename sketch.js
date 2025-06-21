1b57dca867ade3a84a979a860b28de9e02901268
Robinson lopez <robinsonlopez1199@gmail.com>
2025-06-20 19:39:12 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -45,7 +45,7 @@ function setup() {
 
   // Textarea para idea
   let ideaInput = createElement('textarea', '');
-  ideaInput.attribute('placeholder', 'Describe tu idea aquí, pulsa enviar cotizacion, espera unos segundos mientras se carga, deja num de contacto');
+  ideaInput.attribute('placeholder', 'Describe tu idea aquí, pulsa enviar cotizacion, espera unos segundos....');
   ideaInput.parent(formulario);
   ideaInput.style('margin-top', '10px');
   ideaInput.style('width', '100%');
